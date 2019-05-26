f9b1c104f554837862acde1fe2b81d0bbeafb028
mans0n <mans0n@gorani.run>
2019-05-26 13:12:52 +0900

    ar71xx: trim DW02-412H mach file

--- target/linux/ar71xx/files/arch/mips/ath79/mach-dw02-412h.c
+++ target/linux/ar71xx/files/arch/mips/ath79/mach-dw02-412h.c
@@ -6,7 +6,7 @@
  * Copyright (c) 2012 Qualcomm Atheros
  * Copyright (c) 2012-2013 Gabor Juhos <juhosg@openwrt.org>
  * Copyright (c) 2013 Andre Valentin <avalentin@marcant.net>
- * Copyright (c) 2018 mans0n <mans0n@gorani.run>
+ * Copyright (c) 2018 Sungbo Eo <mans0n@gorani.run>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -43,23 +43,11 @@
 #include "dev-wmac.h"
 #include "machtypes.h"
 
-// #define DW02_412H_GPIO_LED_INTERNET		18
-// #define DW02_412H_GPIO_LED_POWER		15
-// #define DW02_412H_GPIO_LED_USB1			4
-// #define DW02_412H_GPIO_LED_USB2			13
-// #define DW02_412H_GPIO_LED_WIFI2G		19
-// #define DW02_412H_GPIO_LED_WIFI5G		17
-// #define DW02_412H_GPIO_LED_WPS			21
 #define DW02_412H_GPIO_LED_WAN			22
 #define DW02_412H_GPIO_LED_WIFI			13
 
 #define DW02_412H_GPIO_BTN_RESET		17
-// #define DW02_412H_GPIO_BTN_RFKILL		1
-// #define DW02_412H_GPIO_BTN_USB1			0
-// #define DW02_412H_GPIO_BTN_USB2			14
 #define DW02_412H_GPIO_BTN_WPS			16
-
-// #define DW02_412H_GPIO_USB_POWER		16
 
 #define DW02_412H_KEYS_POLL_INTERVAL		20	/* msecs */
 #define DW02_412H_KEYS_DEBOUNCE_INTERVAL	(3 * DW02_412H_KEYS_POLL_INTERVAL)
@@ -69,47 +57,7 @@
 #define DW02_412H_WMAC_CALDATA_OFFSET		0x1000
 #define DW02_412H_PCIE_CALDATA_OFFSET		0x5000
 
-/* NBG6616 has a different GPIO usage as it does not have USB Buttons */
-// #define NBG6616_GPIO_LED_USB0			14
-// #define NBG6616_GPIO_LED_USB1			21
-// #define NBG6616_GPIO_LED_WPS			0
-
 static struct gpio_led dw02_412h_leds_gpio[] __initdata = {
-	// {
-	// 	.name		= "dw02_412h:white:internet",
-	// 	.gpio		= DW02_412H_GPIO_LED_INTERNET,
-	// 	.active_low	= 1,
-	// },
-	// {
-	// 	.name		= "dw02_412h:white:power",
-	// 	.gpio		= DW02_412H_GPIO_LED_POWER,
-	// 	.active_low	= 1,
-	// },
-	// {
-	// 	.name		= "dw02_412h:white:usb1",
-	// 	.gpio		= DW02_412H_GPIO_LED_USB1,
-	// 	.active_low	= 1,
-	// },
-	// {
-	// 	.name		= "dw02_412h:white:usb2",
-	// 	.gpio		= DW02_412H_GPIO_LED_USB2,
-	// 	.active_low	= 1,
-	// },
-	// {
-	// 	.name		= "dw02_412h:white:wifi2g",
-	// 	.gpio		= DW02_412H_GPIO_LED_WIFI2G,
-	// 	.active_low	= 1,
-	// },
-	// {
-	// 	.name		= "dw02_412h:white:wifi5g",
-	// 	.gpio		= DW02_412H_GPIO_LED_WIFI5G,
-	// 	.active_low	= 1,
-	// },
-	// {
-	// 	.name		= "dw02_412h:white:wps",
-	// 	.gpio		= DW02_412H_GPIO_LED_WPS,
-	// 	.active_low	= 1,
-	// }
 	{
 		.name		= "dw02-412h:green:wan",
 		.gpio		= DW02_412H_GPIO_LED_WAN,
@@ -131,30 +79,6 @@ static struct gpio_keys_button dw02_412h_gpio_keys[] __initdata = {
 		.gpio		= DW02_412H_GPIO_BTN_RESET,
 		.active_low	= 1,
 	},
-	// {
-	// 	.desc		= "RFKILL button",
-	// 	.type		= EV_SW,
-	// 	.code		= KEY_RFKILL,
-	// 	.debounce_interval = DW02_412H_KEYS_DEBOUNCE_INTERVAL,
-	// 	.gpio		= DW02_412H_GPIO_BTN_RFKILL,
-	// 	.active_low	= 0,
-	// },
-	// {
-	// 	.desc		= "USB1 eject button",
-	// 	.type		= EV_KEY,
-	// 	.code		= BTN_1,
-	// 	.debounce_interval = DW02_412H_KEYS_DEBOUNCE_INTERVAL,
-	// 	.gpio		= DW02_412H_GPIO_BTN_USB1,
-	// 	.active_low	= 1,
-	// },
-	// {
-	// 	.desc		= "USB2 eject button",
-	// 	.type		= EV_KEY,
-	// 	.code		= BTN_2,
-	// 	.debounce_interval = DW02_412H_KEYS_DEBOUNCE_INTERVAL,
-	// 	.gpio		= DW02_412H_GPIO_BTN_USB2,
-	// 	.active_low	= 1,
-	// },
 	{
 		.desc		= "WPS button",
 		.type		= EV_KEY,
@@ -164,69 +88,6 @@ static struct gpio_keys_button dw02_412h_gpio_keys[] __initdata = {
 		.active_low	= 1,
 	},
 };
-
-
-
-// static struct gpio_led nbg6616_leds_gpio[] __initdata = {
-// 	{
-// 		.name		= "nbg6616:green:power",
-// 		.gpio		= DW02_412H_GPIO_LED_POWER,
-// 		.active_low	= 1,
-// 	},
-// 	{
-// 		.name		= "nbg6616:green:usb2",
-// 		.gpio		= NBG6616_GPIO_LED_USB0,
-// 		.active_low	= 1,
-// 	},
-// 	{
-// 		.name		= "nbg6616:green:usb1",
-// 		.gpio		= NBG6616_GPIO_LED_USB1,
-// 		.active_low	= 1,
-// 	},
-// 	{
-// 		.name		= "nbg6616:green:wifi2g",
-// 		.gpio		= DW02_412H_GPIO_LED_WIFI2G,
-// 		.active_low	= 1,
-// 	},
-// 	{
-// 		.name		= "nbg6616:green:wifi5g",
-// 		.gpio		= DW02_412H_GPIO_LED_WIFI5G,
-// 		.active_low	= 1,
-// 	},
-// 	{
-// 		.name		= "nbg6616:green:wps",
-// 		.gpio		= NBG6616_GPIO_LED_WPS,
-// 		.active_low	= 1,
-// 	}
-// };
-
-// static struct gpio_keys_button nbg6616_gpio_keys[] __initdata = {
-// 	{
-// 		.desc		= "RESET button",
-// 		.type		= EV_KEY,
-// 		.code		= KEY_RESTART,
-// 		.debounce_interval = DW02_412H_KEYS_DEBOUNCE_INTERVAL,
-// 		.gpio		= DW02_412H_GPIO_BTN_RESET,
-// 		.active_low	= 1,
-// 	},
-// 	{
-// 		.desc		= "RFKILL button",
-// 		.type		= EV_KEY,
-// 		.code		= KEY_RFKILL,
-// 		.debounce_interval = DW02_412H_KEYS_DEBOUNCE_INTERVAL,
-// 		.gpio		= DW02_412H_GPIO_BTN_RFKILL,
-// 		.active_low	= 1,
-// 	},
-// 	{
-// 		.desc		= "WPS button",
-// 		.type		= EV_KEY,
-// 		.code		= KEY_WPS_BUTTON,
-// 		.debounce_interval = DW02_412H_KEYS_DEBOUNCE_INTERVAL,
-// 		.gpio		= DW02_412H_GPIO_BTN_WPS,
-// 		.active_low	= 1,
-// 	},
-// };
-
 
 static struct ar8327_pad_cfg dw02_412h_ar8327_pad0_cfg;
 static struct ar8327_pad_cfg dw02_412h_ar8327_pad6_cfg;
@@ -270,8 +131,6 @@ static void __init dw02_412h_setup(void)
 	u8 *art = (u8 *) KSEG1ADDR(0x1fff0000);
 	u8 tmp_wmac[ETH_ALEN];
 
-	///////////////////////////////////////////////////////////////////////
-
 	/* GMAC0 of the AR8337 switch is connected to GMAC0 via RGMII */
 	dw02_412h_ar8327_pad0_cfg.mode = AR8327_PAD_MAC_RGMII;
 	dw02_412h_ar8327_pad0_cfg.txclk_delay_en = true;
@@ -294,8 +153,6 @@ static void __init dw02_412h_setup(void)
 	dw02_412h_ar8327_led_cfg.led_ctrl2 = 0xffb7ffb7;
 	dw02_412h_ar8327_led_cfg.led_ctrl3 = 0x03ffff00;
 
-	///////////////////////////////////////////////////////////////////////
-
 	ath79_register_m25p80(NULL);
 
 	ath79_register_leds_gpio(-1, leds_num, leds);
@@ -305,15 +162,12 @@ static void __init dw02_412h_setup(void)
 	ath79_nfc_set_ecc_mode(AR934X_NFC_ECC_HW);
 	ath79_register_nfc();
 
-	// gpio_request_one(DW02_412H_GPIO_USB_POWER,
-	// 	GPIOF_OUT_INIT_HIGH | GPIOF_EXPORT_DIR_FIXED,
-	// 	"USB power");
-
 	ath79_register_usb();
 
 	ath79_init_mac(&tmp_wmac[0], art + DW02_412H_MAC1_OFFSET, 2);
 
 	ath79_register_pci();
+	// ap91_pci_init(art + DW02_412H_PCIE_CALDATA_OFFSET, NULL);
 
 	ath79_register_wmac(art + DW02_412H_WMAC_CALDATA_OFFSET, tmp_wmac);
 
@@ -340,8 +194,6 @@ static void __init dw02_412h_setup(void)
 	ath79_eth1_data.duplex = DUPLEX_FULL;
 
 	ath79_register_eth(1);
-
-	// ap91_pci_init(art + DW02_412H_PCIE_CALDATA_OFFSET, NULL);
 }
 
 MIPS_MACHINE(ATH79_MACH_DW02_412H, "DW02-412H",
