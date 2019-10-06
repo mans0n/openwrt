5cd171e94b06ddeed77c2d7423108fcd93c292b8
mans0n <mans0n@gorani.run>
2019-10-07 01:54:52 +0900

    ath79: add preliminary support for Dongwon TNI DW02-412H

--- target/linux/ath79/image/nand.mk
+++ target/linux/ath79/image/nand.mk
@@ -1,3 +1,29 @@
+define Build/dongwon-tni_dw02-412h_header
+  dd if=$@ of=$@.new bs=1 count=4
+  echo -ne '\x00\x00\x00\x00\x00\x03\x00\x00' | dd of=$@.new bs=1 seek=4 conv=notrunc
+  dd if=$@ of=$@.new bs=1 count=56 skip=8 seek=12 conv=notrunc
+  crc32 $@.new | xxd -r -p | dd of=$@.new bs=1 seek=4 conv=notrunc
+  dd if=$@ of=$@.new bs=4k skip=64 iflag=skip_bytes oflag=append conv=notrunc
+  mv $@.new $@
+endef
+
+define Device/dongwon-tni_dw02-412h
+  ATH_SOC := qca9557
+  DEVICE_VENDOR := Dongwon TNI
+  DEVICE_MODEL := DW02-412H
+  DEVICE_PACKAGES := kmod-ath10k-ct ath10k-firmware-qca988x-ct kmod-usb2
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  KERNEL_SIZE = 2048k
+  IMAGE_SIZE := 131072k
+  UIMAGE_NAME := ISQ-4000
+  KERNEL := kernel-bin | append-dtb | lzma | uImage lzma | dongwon-tni_dw02-412h_header
+  KERNEL_INITRAMFS := $$(KERNEL)
+  IMAGES := sysupgrade.tar
+  IMAGE/sysupgrade.tar := sysupgrade-tar
+endef
+TARGET_DEVICES += dongwon-tni_dw02-412h
+
 define Device/glinet_gl-ar300m-nand
   ATH_SOC := qca9531
   DEVICE_VENDOR := GL.iNet
